7baa4f3e35107f8872168fe5eb9ad685146cf5af
rdvnabay <ridvanabay@gmail.com>
2022-02-07 23:18:37 +0300

    day 1 completed

---
+++ 01_day_Introduction/main.js
@@ -0,0 +1,5 @@
+//Başka script dosyasında tanımlanan global değişkenler farklı script dosyalarında direkt çağrılabilir.
+
+console.log(firstName, lastName, country, city)
+console.log(gravity, PI)
+console.log(name, job, live);scrollbars
---
+++ 01_day_Introduction/variable.js
@@ -0,0 +1,13 @@
+let firstName = 'Rıdvan'
+let lastName = 'Abay'
+let country = 'Turkey'
+let city = 'Istanbul'
+
+
+const gravity = 9.81
+const PI = 3.14
+
+
+let name = 'Berkan', 
+    job = 'Teacher', 
+    live = 'Turkey'
